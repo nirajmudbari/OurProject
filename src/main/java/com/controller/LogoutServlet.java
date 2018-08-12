130bc2503678013224a1048ff1a76dfdece37add
Niraj Mudbari <niraaz2010@gmail.com>
2018-08-12 16:50:01 +0545

    first commit

---
+++ src/main/java/com/controller/LogoutServlet.java
@@ -0,0 +1,30 @@
+package com.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/logout.do")
+public class LogoutServlet extends HttpServlet {
+	public LogoutServlet() {
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		HttpSession session = request.getSession();
+		session.invalidate();
+
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+
+	}
+}
